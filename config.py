e2bc47d801e38c56b4058cb1498e38f4a5687524
AlanSilvaaa <alan@patagon>
2026-05-02 22:01:33 -0400

    chore: test with gemma-3-270m

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-MODEL_ID = "google/gemma-3-1b-it"
-LORA_DIR = "./checkpoints/gemma-3-1b-it-gsm8k-lora"
+MODEL_ID = "google/gemma-3-270m-it"
+LORA_DIR = "./checkpoints/gemma-3-270-it-gsm8k-lora"
+
+# MODEL_ID = "google/gemma-3-1b-it"
+# LORA_DIR = "./checkpoints/gemma-3-1b-it-gsm8k-lora"
 
 # MODEL_ID = "google/gemma-3-12b-it"
 # LORA_DIR = "./checkpoints/gemma-3-12b-it-gsm8k-lora"
